bdef98c0368bf40d66f526fffbf82762f9f83588
aloneturing <aaadityamishra29@gmail.com>
2024-11-20 13:59:48 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+            color: 'inherit',
+            a: {
+              color: 'inherit',
+              textDecoration: 'underline',
+              '&:hover': {
+                color: '#6366f1',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
